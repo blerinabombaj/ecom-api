e38ad9b44702f156124925c0cf465393577f9ab7
Robert Lleshi <robertlleshi@192.168.1.5>
2025-11-30 02:02:31 +0100

    Add /metrics endpoint for Prometheus

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Ecom API Live!'));
+app.get('/metrics', (req, res) => {
+  res.send('# HELP requests_total Total requests\n# TYPE requests_total counter\nrequests_total 123');
+});
 app.listen(3000, () => console.log('Running on 3000'));
